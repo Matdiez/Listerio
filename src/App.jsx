e2287309c4379675cdcb24238623f5ac7f300cec
Marcos <TheMatdiez@gmail.com>
2023-04-13 21:50:21 -0300

    adding tasks feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,61 @@
-import './App.css'
-import Form from './components/Form'
-import Task from './components/Task'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import Task from "./components/Task";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    setTasks(tasks);
+  }, []);
+
+  function addTask(name) {
+    setTasks((prev) => {
+      return [...prev, { name: name, done: false }];
+    });
+  }
+
+  function updateCheck(taskIndex, newDone) {
+    setTasks((prev) => {
+      const newTasks = [...prev];
+      newTasks[taskIndex].done = newDone;
+      return newTasks;
+    });
+  }
+
+  const tasksCompleted = tasks.filter((t) => t.done).length;
+  const tasksTotal = tasks.length;
+  const percentage = (tasksCompleted / tasksTotal) * 100;
+
+  function getMessage() {
+    if (percentage === 0) {
+      return "Try to do at least one!";
+    }
+    if (percentage === 100) {
+      return "Nice job!";
+    }
+    return "Keep going, you can do it!";
+  }
+
   return (
     <main>
-      <Form />
-      <Task />
+      <h1>
+        Completed {tasksCompleted}/{tasksTotal}
+      </h1>
+      <h2>{getMessage()}</h2>
+      <Form onAdd={addTask} />
+      {tasks.map((task, index) => (
+        <Task {...task} onToggle={(done) => updateCheck(index, done)} />
+      ))}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 
-export default function Form() {
+export default function Form({ onAdd }) {
   const [task, setTask] = useState("");
+  function handleSubmit(ev) {
+    ev.preventDefault();
+    onAdd(task);
+    setTask("");
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <button>+</button>
-      <input type="text" value={task} onChange={ev => setTask(ev.target.value)} placeholder="New Task" />
+      <input
+        type="text"
+        value={task}
+        onChange={(ev) => setTask(ev.target.value)}
+        placeholder="New Task"
+        required
+      />
     </form>
   );
 }
